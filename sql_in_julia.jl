3c17d2651fd8c626af2e97d97c9f531f817fc354
Aureum01 <71108402+Aureum01@users.noreply.github.com>
2022-11-29 19:28:45 -0500

    Create sql_in_julia.jl

---
+++ sql_in_julia.jl
@@ -0,0 +1,12 @@
+using Pkg
+
+Pkg.add("SQLite");
+Pkg.add("DataFrames");
+
+using SQLite; 
+using DataFrames;
+
+db = SQLite.DB("data/example.sqlite"); #Database Connect
+con = DBInterface; #connection
+
+df = con.execute(db, 'SELECT * FROM example") |> DataFrame; #To list from example
